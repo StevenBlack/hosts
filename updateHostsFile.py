31ea0fc25b0c3095864d186d1d42f4d94ca4794a
Steven Black <steveb@stevenblack.com>
2016-03-13 11:24:17 -0400

    Fixes #102.

--- updateHostsFile.py
+++ updateHostsFile.py
@@ -292,7 +292,7 @@ def removeDupsAndExcl(mergeFile):
         os.makedirs(outputPath)
 
     # Another mode is required to read and write the file in Python 3
-    finalFile = open(os.path.join(outputPath, 'hosts'), 'w+')
+    finalFile = open(os.path.join(outputPath, 'hosts'), 'r+')
     mergeFile.seek(0) # reset file pointer
 
     hostnames = set()
