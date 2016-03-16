190a53c4cfbf987ba2706dca390d9e56735fe204
Steven Black <steveb@stevenblack.com>
2016-03-16 00:33:49 -0400

    Note the number of unique domains in each hosts file's header.

--- updateHostsFile.py
+++ updateHostsFile.py
@@ -370,7 +370,8 @@ def writeOpeningHeader(finalFile):
     finalFile.seek(0) #write at the top
     writeData(finalFile, '# This hosts file is a merged collection of hosts from reputable sources,\n')
     writeData(finalFile, '# with a dash of crowd sourcing via Github\n#\n')
-    writeData(finalFile, '# Date: ' + time.strftime("%B %d %Y", time.gmtime()) + '\n#\n')
+    writeData(finalFile, '# Date: ' + time.strftime("%B %d %Y", time.gmtime()) + '\n')
+    writeData(finalFile, '# Number of unique domains: ' + "{:,}".format(numberOfRules) + '\n#\n')
     writeData(finalFile, '# Fetch the latest version of this file: https://raw.githubusercontent.com/StevenBlack/hosts/master/hosts\n')
     writeData(finalFile, '# Project home page: https://github.com/StevenBlack/hosts\n#\n')
     writeData(finalFile, '# ===============================================================\n')
