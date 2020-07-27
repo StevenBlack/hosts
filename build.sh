e7f49752ca07120032b74e780d9e7d793546b0e9
newsworthy39 <newsworthy39@github.com>
2020-07-27 12:43:03 +0200

    Added dnsmasq dockerfile

--- build.sh
+++ build.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
+docker run --rm --privileged multiarch/qemu-user-static --reset -p yes
 docker run -v /tmp/app/build:/tmp/app/build  -it test:test python3 updateHostsFile.py --ip 10.0.92.1 --auto -n -s -e fakenews -o build
