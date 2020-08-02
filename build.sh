1c15888bb34f2cfc8d372c98cc7c996bd95c8313
newsworthy39 <newsworthy39@github.com>
2020-08-02 17:51:02 +0200

    Added stuff

--- build.sh
+++ build.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
 docker build -t hosts:builder -f Dockerfile .
-docker run -v $PWD/build:/tmp/app/build  -it hosts:builder python3 updateHostsFile.py --ip 10.0.92.1 --auto -n -s -e fakenews -o build
+docker run -v $PWD/build:/tmp/app/build  -it hosts:builder python3 updateHostsFile.py --ip 10.0.92.1 -a -n -s -e fakenews -o build 
 
 echo switching to remote peer
 eval $(docker-machine env bananapi)
 docker build -t dnsmasq:latest -f dnsmasqDockerfile .
 docker stop dnsmasq 
 docker rm dnsmasq
-docker run --name dnsmasq -p 53:53/udp -d dnsmasq:latest
-
+docker run --name dnsmasq --network host --dns 1.1.1.1 --restart always -d dnsmasq:latest
