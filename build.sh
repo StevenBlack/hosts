e3079676468a78e152f4f67879e2a9507bbbbf78
newsworthy39 <newsworthy39@github.com>
2020-08-30 13:51:38 +0200

    Updated paths

--- build.sh
+++ build.sh
@@ -1,10 +1,18 @@
 #!/bin/bash
+
+echo "Fetching source"
+git pull origin master
+
 echo "Building build-context"
 docker build -t hosts:builder -f Dockerfile .
-docker run -v $PWD/build:/tmp/app/build  -it hosts:builder python3 updateHostsFile.py --ip 10.0.92.1 -a -n -s -e fakenews -o build 
+docker run -v $PWD/build:/tmp/app/build  -it hosts:builder python3 updateHostsFile.py --ip $(dig mjay.engineering +short) -a -n -s -e fakenews -e gambling -o build -b
 
 echo "Building container, with source-code generated above"
 docker build -t dnsmasq:latest -f dnsmasqDockerfile .
 docker stop dnsmasq 
 docker rm dnsmasq
 docker run --name dnsmasq --network host --dns 1.1.1.1 --restart always -d dnsmasq:latest
+
+sudo chown -R gandalf:users .
+
+docker image prune -af
