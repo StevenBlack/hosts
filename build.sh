fcaed5c361e465e5719a32d10dd304212a6daad1
newsworthy39 <newsworthy39@github.com>
2020-08-30 15:13:26 +0200

    Moved IP into environment

--- build.sh
+++ build.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 
+echo "Using DOCKER_ADBLOCK_IP ${DOCKER_ADBLOCK_IP}"
+
 echo "Fetching source"
 #git pull origin master
 
 echo "Building build-context"
 docker build -t hosts:builder -f Dockerfile .
-docker run -v $PWD:/tmp/app  -it hosts:builder python3 updateHostsFile.py --ip $(dig mjay.engineering +short) -a -n -s -e fakenews -e gambling --blacklist blacklist -o build -b
+docker run -v $PWD:/tmp/app  -it hosts:builder python3 updateHostsFile.py --ip ${DOCKER_ADBLOCK_IP} -a -n -s -e fakenews -e gambling --blacklist blacklist -o build -b
 
 echo "Building container, with source-code generated above"
 docker build -t dnsmasq:latest -f dnsmasqDockerfile .
