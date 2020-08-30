3607f18023d66361d78d12cb4f5bf6a0b6ed90b7
newsworthy39 <newsworthy39@github.com>
2020-08-30 15:22:45 +0200

    Cleanup, to make it more readable

--- build.sh
+++ build.sh
@@ -1,18 +1,14 @@
 #!/bin/bash
 
 echo "Using DOCKER_ADBLOCK_IP ${DOCKER_ADBLOCK_IP}"
-
-echo "Fetching source"
-#git pull origin master
-
-echo "Building build-context"
+echo "* Building build-context, to use"
 docker build -t hosts:builder -f Dockerfile .
 docker run -v $PWD:/tmp/app  -it hosts:builder python3 updateHostsFile.py --ip ${DOCKER_ADBLOCK_IP} -a -n -s -e fakenews -e gambling --blacklist blacklist -o build -b
 
-echo "Building container, with source-code generated above"
+echo "* Building container, with source-code generated above"
 docker build -t dnsmasq:latest -f dnsmasqDockerfile .
+
+echo "* Deploying new container"
 docker stop dnsmasq 
 docker rm dnsmasq
 docker run --name dnsmasq --network host --dns 1.1.1.1 --restart always -d dnsmasq:latest
-
-docker image prune -af
