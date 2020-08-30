f208d750bfc4ebfa937b6a5e8d8479a4eddf84e2
newsworthy39 <newsworthy39@github.com>
2020-08-30 15:25:37 +0200

    Remove itneractive

--- build.sh
+++ build.sh
@@ -3,7 +3,7 @@
 echo "Using DOCKER_ADBLOCK_IP ${DOCKER_ADBLOCK_IP}"
 echo "* Building build-context, to use"
 docker build -t hosts:builder -f Dockerfile .
-docker run -v $PWD:/tmp/app  -it hosts:builder python3 updateHostsFile.py --ip ${DOCKER_ADBLOCK_IP} -a -n -s -e fakenews -e gambling --blacklist blacklist -o build -b
+docker run -v $PWD:/tmp/app  hosts:builder python3 updateHostsFile.py --ip ${DOCKER_ADBLOCK_IP} -a -n -s -e fakenews -e gambling --blacklist blacklist -o build -b
 
 echo "* Building container, with source-code generated above"
 docker build -t dnsmasq:latest -f dnsmasqDockerfile .
