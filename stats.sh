4cfe084f40bf5366ba2733690253881662d0f5fd
Steven Black <steveb@stevenblack.com>
2022-07-18 22:49:04 -0400

    Add the beginnings of statistics tracking.

---
+++ stats.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+echo \n "" > stats.out
+for TAG_DATE in $(git tag --sort=creatordate  --format='%(refname:short),%(creatordate:short)'); do
+    # echo "$TAGDATA"
+    split=(${TAG_DATE//,/ })
+    # echo ${split[0]}  
+    git checkout tags/${split[0]} readmeData.json 
+    entries=$(jq '.base.entries' readmeData.json)
+    echo ${split[1]},${entries} >> stats.out
+done
+cat stats.out
+
