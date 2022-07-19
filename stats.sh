410aca3cc3677658a130e1e50eb4b2026d79ce26
Steven Black <steveb@stevenblack.com>
2022-07-18 23:09:53 -0400

    Issue #2104: fix — Remove cruft in the stats bash script.

--- stats.sh
+++ stats.sh
@@ -1,12 +1,10 @@
 #!/usr/bin/env bash
 echo \n "" > stats.out
 for TAG_DATE in $(git tag --sort=creatordate  --format='%(refname:short),%(creatordate:short)'); do
-    # echo "$TAGDATA"
+    # echo "$TAG_DATE"
     split=(${TAG_DATE//,/ })
     # echo ${split[0]}  
     git checkout tags/${split[0]} readmeData.json 
     entries=$(jq '.base.entries' readmeData.json)
     echo ${split[1]},${entries} >> stats.out
 done
-cat stats.out
-
