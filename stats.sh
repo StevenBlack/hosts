77946b508181832816d2b6677739700506a3e248
EmilySeville7cfg <EmilySeville7cfg@gmail.com>
2022-12-13 01:46:27 +1000

    Remove commented code: - git exists for keeping all code

--- stats.sh
+++ stats.sh
@@ -1,9 +1,7 @@
 #!/usr/bin/env bash
 echo > stats.out
 for TAG_DATE in $(git tag --sort=creatordate  --format='%(refname:short),%(creatordate:short)'); do
-    # echo "$TAG_DATE"
     split=(${TAG_DATE//,/ })
-    # echo ${split[0]}  
     git checkout tags/${split[0]} readmeData.json 
     entries=$(jq '.base.entries' readmeData.json)
     echo ${split[1]},${entries} >> stats.out
