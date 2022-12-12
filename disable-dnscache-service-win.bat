df9574507b3aba7838281262ab6a05eccb1ebdd4
EmilySeville7cfg <EmilySeville7cfg@gmail.com>
2022-12-13 01:29:40 +1000

    Use procedures instead of plaing `goto`s in: - `disable-dnscache-service-win.bat`

--- disable-dnscache-service-win.bat
+++ disable-dnscache-service-win.bat
@@ -14,30 +14,32 @@ if "%PROCESSOR_ARCHITECTURE%" equ "amd64" (
 :: If the error flag set, we do not have admin rights.
 if %ERRORLEVEL% neq 0 (
     echo Requesting administrative privileges...
-    goto UACPrompt
+    call :UACPrompt
 ) else (
-    goto gotAdmin
+    call :gotAdmin
 )
 
-:UACPrompt
-echo Set UAC = CreateObject^("Shell.Application"^) > "%TEMP%\getadmin.vbs"
-set params= %*
-echo UAC.ShellExecute "cmd.exe", "/c ""%~s0"" %params:"=""%", "", "runas", 1 >> "%TEMP%\getadmin.vbs"
+exit /b 0
 
-wscript.exe "%TEMP%\getadmin.vbs"
-del "%TEMP%\getadmin.vbs"
+
+:UACPrompt
+    echo Set UAC = CreateObject^("Shell.Application"^) > "%TEMP%\getadmin.vbs"
+    set params= %*
+    echo UAC.ShellExecute "cmd.exe", "/c ""%~s0"" %params:"=""%", "", "runas", 1 >> "%TEMP%\getadmin.vbs"
+
+    wscript.exe "%TEMP%\getadmin.vbs"
+    del "%TEMP%\getadmin.vbs"
 exit /b
 
 :gotAdmin
-
-:SCset
-:: VALUE
-:: 2 (Automatic) (DEFAULT)
-:: 4 (Disabled) (to prevent network freeze after applying a huge hosts file)
-::
-:: See https://superuser.com/a/1277960
-::
-reg add "HKLM\SYSTEM\CurrentControlSet\services\Dnscache" /v Start /t REG_DWORD /d 4 /f
-echo Reboot your system now!
-echo.
-pause
+    :: VALUE
+    :: 2 (Automatic) (DEFAULT)
+    :: 4 (Disabled) (to prevent network freeze after applying a huge hosts file)
+    ::
+    :: See https://superuser.com/a/1277960
+    ::
+    reg add "HKLM\SYSTEM\CurrentControlSet\services\Dnscache" /v Start /t REG_DWORD /d 4 /f
+    echo Reboot your system now!
+    echo.
+    pause
+exit /b 0
