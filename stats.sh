8e62205909916286efb2e57deb16a58b927b0c6e
EmilySeville7cfg <EmilySeville7cfg@gmail.com>
2022-12-13 01:51:58 +1000

    Replace word splitting with `read` and: - use quotes to protect from word splitting

--- stats.sh
+++ stats.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 echo > stats.out
 for TAG_DATE in $(git tag --sort=creatordate  --format='%(refname:short),%(creatordate:short)'); do
-    split=(${TAG_DATE//,/ })
-    git checkout tags/${split[0]} readmeData.json 
-    entries=$(jq '.base.entries' readmeData.json)
-    echo ${split[1]},${entries} >> stats.out
+    IFS="," read -r -a split <<< "$TAG_DATE"
+    git checkout "tags/${split[0]}" readmeData.json 
+    entries="$(jq '.base.entries' readmeData.json)"
+    echo "${split[1]},${entries}" >> stats.out
 done
