b62a027361bf7b37a51ec66e3a38c978a843b540
Yethal <26117918+Yethal@users.noreply.github.com>
2025-03-03 15:58:28 +0100

    Add generated unbound.conf files as packages

--- flake.nix
+++ flake.nix
@@ -1,22 +1,47 @@
 {
   description = "Unified hosts file with base extensions.";
-  outputs = { self, nixpkgs, ... }@inputs:
+  outputs =
+    { self, nixpkgs, ... }@inputs:
     let
       forAllSystems = nixpkgs.lib.genAttrs nixpkgs.lib.platforms.unix;
+      toUnboundConf = (
+        file:
+        builtins.concatStringsSep "\n" (
+          builtins.map (
+            line:
+            if (nixpkgs.lib.strings.hasPrefix "#" line) || (line == "") then
+              line
+            else
+              let
+                address = builtins.elemAt (nixpkgs.lib.strings.splitString " " line) 0;
+                domain = builtins.elemAt (nixpkgs.lib.strings.splitString " " line) 1;
+              in
+              ''
+                local-zone: "${domain}" redirect
+                local-data: "${domain} A ${address}"
+              ''
+          ) (nixpkgs.lib.strings.splitString "\n" (builtins.readFile file))
+        )
+      );
 
-      nixpkgsFor = forAllSystems (system: import nixpkgs {
-        inherit system;
-      });
+      nixpkgsFor = forAllSystems (
+        system:
+        import nixpkgs {
+          inherit system;
+        }
+      );
     in
     {
-      nixosModule = { config, ... }:
+      nixosModule =
+        { config, ... }:
         with nixpkgs.lib;
         let
           cfg = config.networking.stevenBlackHosts;
-          alternatesList = (if cfg.blockFakenews then [ "fakenews" ] else []) ++
-                           (if cfg.blockGambling then [ "gambling" ] else []) ++
-                           (if cfg.blockPorn then [ "porn" ] else []) ++
-                           (if cfg.blockSocial then [ "social" ] else []);
+          alternatesList =
+            (if cfg.blockFakenews then [ "fakenews" ] else [ ])
+            ++ (if cfg.blockGambling then [ "gambling" ] else [ ])
+            ++ (if cfg.blockPorn then [ "porn" ] else [ ])
+            ++ (if cfg.blockSocial then [ "social" ] else [ ]);
           alternatesPath = "alternates/" + builtins.concatStringsSep "-" alternatesList + "/";
         in
         {
@@ -33,14 +58,20 @@
           config = mkIf cfg.enable {
             networking.extraHosts =
               let
-                orig = builtins.readFile ("${self}/" + (if alternatesList != [] then alternatesPath else "") + "hosts");
+                orig = builtins.readFile (
+                  "${self}/" + (if alternatesList != [ ] then alternatesPath else "") + "hosts"
+                );
                 ipv6 = builtins.replaceStrings [ "0.0.0.0" ] [ "::" ] orig;
-              in orig + (optionalString cfg.enableIPv6 ("\n" + ipv6));
+              in
+              orig + (optionalString cfg.enableIPv6 ("\n" + ipv6));
           };
         };
 
-      devShells = forAllSystems (system:
-        let pkgs = nixpkgsFor.${system}; in
+      devShells = forAllSystems (
+        system:
+        let
+          pkgs = nixpkgsFor.${system};
+        in
         {
           default = pkgs.mkShell {
             buildInputs = with pkgs; [
@@ -49,6 +80,37 @@
               python3Packages.requests
             ];
           };
-        });
+        }
+      );
+
+      overlays.default = (
+        final: prev: {
+          unboundconfs = prev.lib.makeScope prev.pkgs.newScope (newscope: self.packages.${prev.system});
+        }
+      );
+
+      packages = forAllSystems (
+        system:
+        let
+          pkgs = nixpkgsFor.${system};
+          dir = ./alternates;
+          lists =
+            (pkgs.lib.attrsets.filterAttrs (k: v: builtins.pathExists v) (
+              pkgs.lib.attrsets.mapAttrs (k: v: dir + "/${k}/hosts") (
+                pkgs.lib.attrsets.filterAttrs (k: v: v == "directory") (builtins.readDir dir)
+              )
+            ))
+            // {
+              all = ./hosts;
+            };
+        in
+        pkgs.lib.attrsets.mapAttrs (
+          k: v:
+          pkgs.writeTextFile {
+            name = k;
+            text = toUnboundConf v;
+          }
+        ) lists
+      );
     };
 }
